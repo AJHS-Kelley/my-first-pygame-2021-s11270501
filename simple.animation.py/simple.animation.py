61208906a475c4540448e106fea0a33ea1f7266e
Victor <s11270501@students.duvalschools.org>
2022-01-24 11:48:26 -0500

    v0.1
    imported modules and set up window

--- simple.animation.py/simple.animation.py
+++ simple.animation.py/simple.animation.py
@@ -1,1 +1,12 @@
 #Simple Animation with Pygame, Ryan Kelley, 12/08/21,10:13AM, v0.0
+
+import pygame, sys,time
+from pygame.locals import *
+
+
+pygame.init()
+
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT),0,32)
+pygame.display.set_caption('Animation Example!')
