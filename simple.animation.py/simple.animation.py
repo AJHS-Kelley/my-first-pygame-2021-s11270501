49b7dd1b5a11aade90a14a20499e18086fd3a693
Victor <s11270501@students.duvalschools.org>
2022-01-24 13:18:43 -0500

    v0.6
    filled in window surface.

--- simple.animation.py/simple.animation.py
+++ simple.animation.py/simple.animation.py
@@ -40,3 +40,21 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit() 
+
+    windowSurface.fill(WHITE)
+
+    for b in boxes:
+
+        if b['dir'] == DOWNLEFT:
+            b['rect'].left -=MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b['dir'] == DOWNRIGHT:
+            b['rect'].left += MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b[ 'dir'] == UPLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top  -= MOVESPEED 
+        if b['dir' ] == UPRIGHT:
+            b['rect'].left += MOVESPEED
+            b['rect'].top -= MOVESPEED
+
